924067a486f3e73005fa37dbebb6ccc2439a693d
Ty Davis <tydavis76@gmail.com>
2026-05-08 21:31:43 +0000

    feat(kb-fitness): 8 active workout screens + ActiveTopBar + ActiveWorkoutRouter

--- kb-fitness/src/tabs/TodayTab.tsx
+++ kb-fitness/src/tabs/TodayTab.tsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import { TodayScreen } from '../screens/TodayScreen'
 import { SessionPreviewScreen } from '../screens/SessionPreviewScreen'
+import { ActiveWorkoutRouter } from '../screens/active/ActiveWorkoutRouter'
 
 export default function TodayTab() {
   return (
     <Routes>
       <Route index element={<TodayScreen />} />
       <Route path="preview" element={<SessionPreviewScreen />} />
-      <Route path="active/*" element={<div style={{ padding: 16, color: '#fff' }}>Active workout (Task 11)</div>} />
+      <Route path="active/*" element={<ActiveWorkoutRouter />} />
       <Route path="recap" element={<div style={{ padding: 16, color: '#fff' }}>Recap (Task 12)</div>} />
     </Routes>
   )
