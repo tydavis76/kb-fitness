1b1f982e531e6217639cc2d74659b75f481a566c
Ty Davis <tydavis76@gmail.com>
2026-05-10 20:00:50 -0500

    chore: update github workflow and base path for kb-fitness standalone

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/rkc-tracker/kb-fitness/',
+  base: '/kb-fitness/',
   plugins: [
     react(),
     VitePWA({
